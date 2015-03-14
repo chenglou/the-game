ca9026be853a650267739de4129df29af879f533
Cheng Lou <chenglou92@gmail.com>
2015-03-14 04:48:54 -0400

    Remove mapeditor remains completely

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,13 @@
 var webpack = require('webpack');
 
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
-
 module.exports = {
   entry: {
     index: './public/index.jsx',
-    editor: './public/src/meta/mapEditor.jsx',
   },
   output: {
     path: './public/out',
-    filename: '[name].js',
+    filename: 'index.js',
   },
-  plugins: [commonsPlugin],
   module: {
     loaders: [
       {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader?experimental'},
