926231eb2ac7aa0b1def44f787749508b373a569
Cheng Lou <chenglou92@gmail.com>
2015-04-04 13:06:31 -0400

    Add attack data; preparing for invasion logic

--- public/src/rankers.js
+++ public/src/rankers.js
@@ -1,9 +1,9 @@
-var arrToSet = require('./utils/arrToSet');
+let arrToSet = require('./utils/arrToSet');
 
-var villageByRank = ['Hovel', 'Town', 'Fort', 'Castle'];
-var villagerByRank = ['Peasant', 'Infantry', 'Soldier', 'Knight'];
+let villageByRank = ['Hovel', 'Town', 'Fort', 'Castle'];
+let villagerByRank = ['Peasant', 'Infantry', 'Soldier', 'Knight'];
 
-var upkeep = {
+let upkeep = {
   Peasant: 2,
   Infantry: 6,
   Soldier: 18,
@@ -12,15 +12,44 @@ var upkeep = {
   Cannon: 5,
 };
 
-var villageCanProduce = {
+let canAttack = {
+  Peasant: [],
+  Infantry: ['Peasant'],
+  Soldier: ['Hovel', 'Town', 'Watchtower', 'Peasant', 'Infantry'],
+  Knight: [
+    'Hovel', 'Town', 'Fort', 'Watchtower', 'Cannon', 'Peasant', 'Infantry',
+    'Soldier',
+  ],
+  Cannon: ['Hovel', 'Town', 'Watchtower', 'Peasant', 'Infantry'],
+  // Watchtower: [],
+};
+
+for (let key in canAttack) {
+  canAttack[key] = arrToSet(canAttack[key]);
+}
+
+let hasAura = {
+  Villager: true,
+  Village: true,
+};
+
+let killable = {
+  Villager: true,
+  Village: true,
+  Cannon: true,
+};
+
+let villageCanProduce = {
   Hovel: ['Peasant', 'Infantry'],
   Town: ['Peasant', 'Infantry', 'Soldier'],
   Fort: ['Peasant', 'Infantry', 'Soldier', 'Knight', 'Cannon'],
   Castle: ['Peasant', 'Infantry', 'Soldier', 'Knight', 'Cannon'],
 };
 
-for (var key in villageCanProduce) {
+for (let key in villageCanProduce) {
   villageCanProduce[key] = arrToSet(villageCanProduce[key]);
 }
 
-module.exports = {villageByRank, villagerByRank, upkeep, villageCanProduce};
+module.exports = {
+  villageByRank, villagerByRank, upkeep, villageCanProduce, hasAura, canAttack, killable
+};
