8590a0d50fe32c4fea2db17a1c9316d45ad6634e
Cheng Lou <chenglou92@gmail.com>
2015-04-04 13:03:43 -0400

    Make randNth to mori collections

--- public/src/utils/randNth.js
+++ public/src/utils/randNth.js
@@ -1,5 +1,12 @@
+'use strict';
+
+var M = require('mori');
+
 function randNth(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  if (arr instanceof Array) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  return M.nth(arr, Math.floor(Math.random() * M.count(arr)));
 }
 
 module.exports = randNth;
